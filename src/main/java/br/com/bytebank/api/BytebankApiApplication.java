648daefaffdfffc7720d8d5c542963f744948a52
LeandroMedvedev <medvedevleandro@outlook.com>
2025-09-05 13:58:14 -0300

    feat: initial project structure from Spring CLI

---
+++ src/main/java/br/com/bytebank/api/BytebankApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bytebank.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytebankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytebankApiApplication.class, args);
+	}
+
+}
